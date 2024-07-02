4d7d47127bb33a63b6f9e3c13106775722d3421a
Vinicius Pedrosa de Oliveira Dantas <43658937+viniciuspod@users.noreply.github.com>
2024-07-02 14:39:53 -0300

    primeiro commit do spring initilz

---
+++ src/test/java/br/com/nutriscare/nutrisCareRulesApi/NutrisCareRulesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nutriscare.nutrisCareRulesApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrisCareRulesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
