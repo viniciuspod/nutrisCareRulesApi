b830183cb1fa68ac7f7b245156652372d6a371f9
Vinicius Pedrosa de Oliveira Dantas <43658937+viniciuspod@users.noreply.github.com>
2024-07-05 17:04:11 -0300

    Colocando a formula da normalização

--- src/main/java/br/com/nutriscare/nutrisCareRulesApi/service/DietService.java
+++ src/main/java/br/com/nutriscare/nutrisCareRulesApi/service/DietService.java
@@ -16,10 +16,26 @@ public class DietService {
 
     public Diet buildDiet(User user){
         try {
-            Double bmi = user.getHealthUsu().getBmi();
-            Double levelGoal = Double.valueOf(user.getLevelGoals()) / 3.0;
-            Double levelActivity = Double.valueOf(user.getLevelPhysicalActivityLevel()) / 3.0;
+            // formula da normalização x = x-min/max-min
+            double normalizedBMI = (user.getHealthUsu().getBmi() - 18.5) / (24.9 - 18.5);
+            double normalizedGoalLevel = (user.getLevelGoals() - 1.0) / (3.0 - 1.0);
+            double normalizedActivityLevel = (user.getLevelPhysicalActivityLevel() - 1.0) / (3.0 - 1.0);
 
+            double goalWeight = 0.3;
+            double activityWeight = 0.3;
+            double bmiWeight = 0.4;
+
+            double score = (normalizedGoalLevel * goalWeight) +
+                    (normalizedActivityLevel * activityWeight) +
+                    (normalizedBMI * bmiWeight);
+
+            if (score > 0.7) {
+                log.info("Recomendamos uma dieta de baixa caloria com alta atividade física.");
+            } else if (score > 0.4) {
+                log.info("Recomendamos uma dieta balanceada com atividade física moderada.");
+            } else {
+                log.info("Recomendamos uma dieta de manutenção com atividade física leve.");
+            }
 
             return null;
         } catch (Exception e) {
